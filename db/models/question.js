7f1833f775c937dddbba35135e9ce0c1ee54ff50
Meschreiber <meschreiber3@gmail.com>
2020-08-08 09:32:09 -0600

    First stab at models

--- db/models/question.js
+++ db/models/question.js
@@ -1,7 +1,26 @@
 'use strict'
 
-const {STRING, INTEGER} = require('sequelize')
+// type Question {
+//     id: ID!
+//     topic: [String] # Geography, music, etc.
+//     text: String!
+//     imageUrl: String
+//     answers: [Answer!]!
+// }
+
+const {STRING, ARRAY} = require('sequelize')
 
 module.exports = db => db.define('questions', {
+  uid: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV1,
+    primaryKey: true
+},
+  topic: ARRAY,
   text: STRING,
+  imageUrl: STRINGs
 })
+
+module.exports.associations = (Question, {Answer}) => {
+  Question.hasMany(Answer, { foreignKey: 'uid' })
+}
