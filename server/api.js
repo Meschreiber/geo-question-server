993530aa71e2f24a47cf26034b49ff0275e26c28
Meschreiber <meschreiber3@gmail.com>
2020-08-08 15:00:24 -0600

    Add association between answers and question

--- server/api.js
+++ server/api.js
@@ -1,10 +1,10 @@
 'use strict'
-const { Question} = require('APP/models')
+const { Question, Answer } = require('APP/models')
 const api = module.exports = require('express').Router()
 
 api
   .get('/questions', (req, res, next) => {
-      console.log('getting questions')
+    console.log('getting questions')
     Question.findAll({})
       .then(questions => res.send(questions))
       .catch(next)
@@ -14,7 +14,16 @@ api
       .then(question => res.send(question))
       .catch(next)
   })
-  
+  .get('/answers/:questionId', (req, res, next) => {
+    Answer.findAll({
+      where: {
+        questionID: req.params.questionID
+      }
+    })
+      .then(question => res.send(question))
+      .catch(next)
+  })
+
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
 
