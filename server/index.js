26f347a625c61b33aee61970371070570da0d6e2
Meschreiber <meschreiber3@gmail.com>
2020-08-09 13:38:30 -0600

    Refactor

--- server/index.js
+++ server/index.js
@@ -1,46 +1,61 @@
-'use strict'
+const express = require('express');
+const app = express();
+const  {Question , Answer }  = require('../models/index.js');
+const port = process.env.PORT || 3000;
 
-const path = require('path')
-const express = require('express')
-const PrettyError = require('pretty-error')
-const app = express()
+app.use(express.urlencoded({ extended: true }));
 
-// const pkg = require('APP')
-// if (!pkg.isProduction && !pkg.isTesting) {
-//   // Logging middleware (dev only)
-//   app.use(require('volleyball'))
-// }
+app.get('/', (req, res, next) => {
+    res.send(`The following endpoints are available:
+   \n/questions 
+    \n/questions/:id
+    \n/answers
+    \n/answers/:questionId
+    `)
+})
+    .get('/questions', (req, res, next) => {
+        Question.findAll()
+            .then(questions => res.send(questions))
+            .catch(next)
+    })
+    .get('/questions/:id', (req, res, next) => {
+        Question.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(question => res.send(question))
+        .catch(next)
+    })
+    .get('/answers', (req, res, next) => {
+        Answer.findAll({
+            // Unless specififying attributes, the query that is executed is SELECT "id", "text", "imageUrl", "valid", "questionId", "createdAt", "updatedAt", "QuestionId" FROM "Answers" AS "Answer";
+            // Even though QuestionId no longer exists on the table
+            // Leading to this error:
+            // SequelizeDatabaseError: column "QuestionId" does not exist
+            // at Query.formatError (/Users/maria/Desktop/geo-question-server/node_modules/sequelize/lib/dialects/postgres/query.js:386:16)
+            // at Query.run (/Users/maria/Desktop/geo-question-server/node_modules/sequelize/lib/dialects/postgres/query.js:87:18)
+            // at processTicksAndRejections (internal/process/task_queues.js:97:5)
+            attributes: ["id", "text", "imageUrl", "valid", "questionId"]
+        })
+            .then(answers => res.send(answers))
+            .catch(next)
+    })
+    .get('/answers/:questionId', (req, res, next) => {
+        Answer.findAll({
+            attributes: ["id", "text", "imageUrl", "valid", "questionId"],
+            where: {
+                questionId: req.params.questionId
+            }
+        })
+            .then(question => res.send(question))
+            .catch(next)
+    })
 
-// Pretty error prints errors all pretty.
-const prettyError = new PrettyError
-// Skip events.js and http.js and similar core node files.
-prettyError.skipNodeFiles()
-// Skip all the trace lines about express' core and sub-modules.
-prettyError.skipPackage('express')
-
-module.exports = app
-  .use('/api', require('./api'))
-  // any requests with an extension (.js, .css, etc.) turn into 404
-  .use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
-      next(err)
-    } else {
-      next()
-    }
-  })
-
-if (module === require.main) {
-  // Start listening only if we're the main module.
-  const server = app.listen(
-    process.env.PORT || 3000,
-    () => {
-      console.log(`--- Started HTTP Server for Geography Questions} ---`)
-      const { address, port } = server.address()
-      const host = address === '::' ? 'localhost' : address
-      const urlSafeHost = host.includes(':') ? `[${host}]` : host
-      console.log(`Listening on http://${urlSafeHost}:${port}`)
-    }
-  )
-}
+const server = app.listen(port, () => {
+    console.log(`--- Started HTTP Server for question-server ---`)
+    const { address, port } = server.address()
+    const host = address === '::' ? 'localhost' : address
+    const urlSafeHost = host.includes(':') ? `[${host}]` : host
+    console.log(`Listening on http://${urlSafeHost}:${port}`)
+});
